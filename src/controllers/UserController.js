24a6ec46f1cc754f497d14a0997124ab0c2fe215
EduardoLangner <eduardodossantos752@gmail.com>
2023-05-20 16:50:14 -0300

    Feat: Create user controller and add jwt/bcrypt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,28 @@
 const UserModel = require('../models/UserModel')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 module.exports = {
 
+    async createUser(req, res){
+
+        try{
+            const {name, cpf, email, password} = req.body
+            const hash = bcrypt.hashSync(password, 10)
+            const user = await UserModel.create({name, cpf, email, password: hash})
+            const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+            console.log(`\nHASH: ${hash}\n`)
+            return res.status(200).json({
+                message: 'User created successfully',
+                user,
+                token: token
+            })
+        }catch(error){
+            if(error.name === 'SequelizeUniqueConstraintError'){
+                return res.status(400).json({error: 'Email already exists'})
+            }else{
+                return res.status(400).json({error: 'Error to create user: ' + error})
+            }
+        }
+    }
 }
